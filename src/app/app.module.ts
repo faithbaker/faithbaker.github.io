c53f8e3c0beaa93abe3a6ada9ac18c5be950666a
Derek Baker <db820017@ohio.edu>
2023-09-27 17:56:41 -0400

    Initial generated Angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// @NgModule({
+//   imports: [
+//     MatToolbar,
+//   ]
+// })
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MatToolbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
